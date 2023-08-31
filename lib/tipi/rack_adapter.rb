2253593cf83980ce1b79cb00bdeec8e2dcaa2651
Florian Dejonckheere <florian@nephroflow.com>
2023-08-31 16:00:41 +0200

    Unwrap GzipStream when necessary

--- lib/tipi/rack_adapter.rb
+++ lib/tipi/rack_adapter.rb
@@ -21,6 +21,8 @@ module Tipi
       def respond(request, (status_code, headers, body))
         headers[':status'] = status_code.to_s
 
+        body = body.instance_variable_get(:@body) if body.is_a? Rack::Deflater::GzipStream
+
         content =
           if body.respond_to?(:to_path)
             File.open(body.to_path, 'rb') { |f| f.read }
